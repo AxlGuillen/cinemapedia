e32059f496af0e7101b52c7b7e86d53dfd184e7b
AxlGuillen <axl1504@hotmail.com>
2023-08-08 10:36:07 -0600

    fin seccion 16

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final int pageIndex;
+  const CustomBottomNavigation({super.key, required this.pageIndex});
+
+  void onItemTapped(BuildContext context, int pageIndex) {
+    switch (pageIndex) {
+      case 0:
+        return context.go('/home/:0');
+      case 1:
+        return context.go('/home/:1');
+      case 2:
+        return context.go('/home/:2');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: pageIndex,
       elevation: 0,
+      onTap: (value) => onItemTapped(context, value),
       items: const [
         BottomNavigationBarItem(
-          icon: Icon( Icons.home_max),
+          icon: Icon(Icons.home_max),
           label: 'Inicio',
         ),
-
         BottomNavigationBarItem(
-          icon: Icon( Icons.label_outlined),
-          label: 'Categorias'
-        ),
-
+            icon: Icon(Icons.label_outlined), label: 'Categorias'),
         BottomNavigationBarItem(
-          icon: Icon( Icons.favorite_outline),
-          label: 'Favoritos'
-        ),
+            icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
       ],
     );
   }
